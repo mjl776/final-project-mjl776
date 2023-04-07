6e4307baf41e6da58d077de4d6768f635d2241c8
Michael Lee <mlee@michaels-mbp.mynetworksettings.com>
2023-04-06 20:36:49 -0400

    set up navbar

--- frontend/pages/api/test.js
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import express from express;
-var router = express.Router();
-
-router.get("/", (res, req) => {
-  res.send("API works");
-})
-
-export default router;
---
+++ frontend/pages/nav.js
@@ -0,0 +1,38 @@
+import {Route, Routes } from "react-router-dom";
+
+export default function Nav() {
+    return (
+        <>
+        <nav className = "navbar">
+            <a href = "/" className = "nav-branding"> Paradise </a>
+                <ul className = "nav-menu">
+                    <li class = "nav-item"> 
+                        <a 
+                            href= "/"
+                            className="nav-link"
+                        >
+                            Home 
+                        </a>                       
+                    </li>
+                    <li className = "nav-item"> 
+                        <a 
+                            href= "/about"
+                            className = "nav-link"
+                        >
+                            About 
+                        </a>  
+                    </li>
+                    <li className = "nav-item"> 
+                        <a 
+                            href= "/work"
+                            className ="nav-link"
+                        >
+                            Work 
+                        </a> 
+                    </li>
+                </ul>
+        </nav>
+        
+        </>
+    )
+}
